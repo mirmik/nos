c04589c4da57b2e309441435bda244b433f1c7c7
mirmik <mirmikns@yandex.ru>
2018-12-21 15:56:37 +0500

    a

--- test/sfinae/main.cpp
+++ test/sfinae/main.cpp
@@ -1,50 +1,28 @@
 #include <iostream>
 #include <utility>
 
-namespace nos
-{
-	class ostream;
-}
+#include <nos/io/ostream.h>
 
-struct nos_ostream_ref {
-	nos::ostream* ref;	
-};
+class test_ostream : public nos::ostream {};
 
-ssize_t nos_print(nos_ostream_ref, auto);
+template <typename>
+struct sfinae_true : std::true_type
+{ };
 
-namespace nos
-{
-	class ostream;
+template <typename T> static auto nos_test_has_implicit_overload(int)
+-> sfinae_true<decltype(
+     nos_print(std::declval<test_ostream&>(), std::declval<T>())
+)>;
 
-	namespace detail
-	{
-		template <typename>
-		struct sfinae_true : std::true_type
-		{ };
+template <typename T> static auto nos_test_has_implicit_overload(...)
+-> std::false_type;
 
-		template <typename T> static auto test_has_implicit_overload(int)
-		-> sfinae_true<decltype(
-            ::nos_print(std::declval<nos_ostream_ref>(), std::declval<T>())
-		)>;
+template <typename T>
+struct nos_has_implicit_overload : decltype(nos_test_has_implicit_overload<T>(0)) { };
 
-		template <typename T> static auto test_has_implicit_overload(...)
-		-> std::false_type;
-	}
-
-	template <typename T>
-	struct has_implicit_overload : decltype(detail::test_has_implicit_overload<T>(0)) { };
-
-}
-
-template<> ssize_t nos_print<int>(nos_ostream_ref, int) {}
-
-void func() {
-	int i = 0;
-	nos_print(nos_ostream_ref(), i);
-}
+ssize_t nos_print(nos::ostream&, const char*) {}
 
 int main() 
 {
-	std::cout << nos::has_implicit_overload<const char*>::value << std::endl;
-
+	std::cout << nos_has_implicit_overload<const char*>::value << std::endl;
 }
