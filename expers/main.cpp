21efea8c37b31a3e4edd07161e505d5c05c0990b
mirmik <mirmikns@yandex.ru>
2018-12-31 22:11:16 +0500

    a

--- expers/main.cpp
+++ expers/main.cpp
@@ -3,6 +3,6 @@
 
 int main ()
 {
-	nos::println(nos::bright_green("Mirmik"));
-	nos::println(COLOR_BRIGHT_GREEN("Mirmik"));
+	nos::println(nos::green("Mirmik"));
+	nos::println(COLOR_GREEN("Mirmik"));
 } 
