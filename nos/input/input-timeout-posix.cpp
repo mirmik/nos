965a9c3ad19b7861a4ea63215232684cd8cdaa77
mirmik <mirmikns@yandex.ru>
2020-08-19 13:55:49 +0300

    'comments'

--- nos/input/input-timeout-posix.cpp
+++ nos/input/input-timeout-posix.cpp
@@ -16,19 +16,20 @@ ssize_t nos::fdlike::read_with_timeout_ms(void* ptr, size_t sz, int ms)
 	struct timeval timeout;
 
 	int rv;
-	FD_ZERO(&set); /* clear the set */
-/*	FD_SET(m_fd, &set); /* add our file descriptor to the set */
+	FD_ZERO(&set);
+	FD_SET(m_fd, &set);
 
-/*	timeout.tv_sec = ms / 1000;
+	timeout.tv_sec = ms / 1000;
 	timeout.tv_usec = (ms % 1000) * 1000;
 
 	rv = select(m_fd + 1, &set, NULL, NULL, &timeout);
 	if (rv == -1)
 		return -1;
-		//perror("select"); /* an error accured */
-/*	else if (rv == 0)
+		//perror("select");
+	else if (rv == 0)
 		return -2;
-		//printf("timeout"); /* a timeout occured */
-/*	else
-		return read( ptr, sz ); /* there was data to read */
-//}
+		//printf("timeout");
+	else
+		return read( ptr, sz );
+}
+*/
