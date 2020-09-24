39ef2e286e9915d1827bfd9fd6c9b5b704cf764d
Mirmik <mirmikns@yandex.ru>
2020-09-24 16:34:12 +0300

    clean

--- nos/print/stdtype.cpp
+++ nos/print/stdtype.cpp
@@ -6,12 +6,12 @@
 
 #include <igris/dprint.h>
 
-ssize_t nos_print(nos::ostream& out, const char* str) 
+ssize_t nos_print(nos::ostream& out, const char* str)
 {
 	return out.write(str, strlen(str));
 }
 
-ssize_t nos_print(nos::ostream& out, igris::buffer buf) 
+ssize_t nos_print(nos::ostream& out, igris::buffer buf)
 {
 	return out.write(buf.data(), buf.size());
 }
@@ -26,29 +26,31 @@ ssize_t nos_print(nos::ostream& out, uint16_t obj) 	{ char buf[48];	u64toa(obj, 
 ssize_t nos_print(nos::ostream& out, uint32_t obj) 	{ char buf[48];	u64toa(obj, buf, 10); return nos_print(out, buf); }
 ssize_t nos_print(nos::ostream& out, uint64_t obj) 	{ char buf[48];	u64toa(obj, buf, 10); return nos_print(out, buf); }
 
-ssize_t nos_print(nos::ostream& out, float32_t obj) 	{ 
-	char buf[48];	
+ssize_t nos_print(nos::ostream& out, float32_t obj)
+{
+	char buf[48];
 #if defined(NOS_USE_IGRIS_NUMCONVERT)
-	f32toa(obj, buf, 5); 
+	f32toa(obj, buf, 5);
 #else
 	sprintf(buf, "%f", obj);
-#endif	
-	return nos_print(out, buf); 
+#endif
+	return nos_print(out, buf);
 }
 
 #ifndef WITHOUT_FLOAT64
-ssize_t nos_print(nos::ostream& out, float64_t obj) 	{ 
-	char buf[48]; 
+ssize_t nos_print(nos::ostream& out, float64_t obj)
+{
+	char buf[48];
 #if defined(NOS_USE_IGRIS_NUMCONVERT)
-	f64toa(obj, buf, 5); 
+	f64toa(obj, buf, 5);
 #else
 	sprintf(buf, "%f", obj);
-#endif	
+#endif
 	return nos_print(out, buf);
 }
 #endif
 
-ssize_t nos_print(nos::ostream& out, const std::string& str) 
+ssize_t nos_print(nos::ostream& out, const std::string& str)
 {
 	return nos_print(out, igris::buffer(str));
 }
