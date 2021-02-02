11c135a67f3fe2667fa7128f4a1ac6f0b541fc3c
mirmik <mirmikns@yandex.ru>
2021-02-02 14:30:48 +0300

    'change stdtypes'

--- nos/print/stdtype.cpp
+++ nos/print/stdtype.cpp
@@ -23,14 +23,18 @@ ssize_t nos_print(nos::ostream& out, igris::buffer buf)
 #endif
 
 ssize_t nos_print(nos::ostream& out, bool obj) 		{ return nos_print(out, obj ? "true" : "false"); }
-ssize_t nos_print(nos::ostream& out, int8_t obj) 	{ char buf[48]; sprintf(buf, "%d", obj); return nos_print(out, buf); }
-ssize_t nos_print(nos::ostream& out, int16_t obj) 	{ char buf[48]; sprintf(buf, "%d", obj); return nos_print(out, buf); }
-ssize_t nos_print(nos::ostream& out, int32_t obj) 	{ char buf[48];	sprintf(buf, "%d", obj); return nos_print(out, buf); }
-ssize_t nos_print(nos::ostream& out, int64_t obj) 	{ char buf[48];	sprintf(buf, "%lld", obj); return nos_print(out, buf); }
-ssize_t nos_print(nos::ostream& out, uint8_t obj) 	{ char buf[48];	sprintf(buf, "%u", obj); return nos_print(out, buf); }
-ssize_t nos_print(nos::ostream& out, uint16_t obj) 	{ char buf[48];	sprintf(buf, "%u", obj); return nos_print(out, buf); }
-ssize_t nos_print(nos::ostream& out, uint32_t obj) 	{ char buf[48];	sprintf(buf, "%u", obj); return nos_print(out, buf); }
-ssize_t nos_print(nos::ostream& out, uint64_t obj) 	{ char buf[48];	sprintf(buf, "%llu", obj); return nos_print(out, buf); }
+
+ssize_t nos_print(nos::ostream& out, signed char obj) 	    { char buf[48]; sprintf(buf, "%d", obj);   return nos_print(out, buf); }
+ssize_t nos_print(nos::ostream& out, signed short obj) 	    { char buf[48]; sprintf(buf, "%d", obj);   return nos_print(out, buf); }
+ssize_t nos_print(nos::ostream& out, signed int obj) 	    { char buf[48];	sprintf(buf, "%d", obj);   return nos_print(out, buf); }
+ssize_t nos_print(nos::ostream& out, signed long obj) 	    { char buf[48];	sprintf(buf, "%ld", obj);  return nos_print(out, buf); }
+ssize_t nos_print(nos::ostream& out, signed long long obj) 	{ char buf[48];	sprintf(buf, "%lld", obj); return nos_print(out, buf); }
+
+ssize_t nos_print(nos::ostream& out, unsigned char obj) 	{ char buf[48]; sprintf(buf, "%u", obj);   return nos_print(out, buf); }
+ssize_t nos_print(nos::ostream& out, unsigned short obj) 	{ char buf[48]; sprintf(buf, "%u", obj);   return nos_print(out, buf); }
+ssize_t nos_print(nos::ostream& out, unsigned int obj) 	    { char buf[48];	sprintf(buf, "%u", obj);   return nos_print(out, buf); }
+ssize_t nos_print(nos::ostream& out, unsigned long obj) 	{ char buf[48];	sprintf(buf, "%lu", obj);  return nos_print(out, buf); }
+ssize_t nos_print(nos::ostream& out, unsigned long long obj){ char buf[48];	sprintf(buf, "%llu", obj); return nos_print(out, buf); }
 
 ssize_t nos_print(nos::ostream& out, float obj)
 {
