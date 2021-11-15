5230a515a7af1ac7b7db8e42680d23da4bcb8987
mirmik <mirmikns@yandex.ru>
2021-11-16 00:33:13 +0300

    'a'

--- nos/inet/tcp_server.h
+++ nos/inet/tcp_server.h
@@ -7,17 +7,20 @@ namespace nos {
 	namespace inet {
 		struct tcp_server : public inet::socket {
 			tcp_server() = default;
-
-			//tcp_server(int port);
+			tcp_server(const tcp_server& oth) = default;
+			tcp_server(tcp_server&& oth) = default;
 			tcp_server(const nos::inet::hostaddr& addr, uint16_t port, int conn = 10);
-            //void listen(int port, int conn = 10);
 
+			tcp_server&operator=(const tcp_server& oth) = default;
+			tcp_server&operator=(tcp_server&& oth) = default;
+
+            inet::tcp_socket accept();
+
+        private:
             int init();
 			int bind(const nos::hostaddr& addr, uint16_t port);
             int listen();
             int listen(int conn);
-
-            inet::tcp_socket accept();
 		};
 	}
 }
