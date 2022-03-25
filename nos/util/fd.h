1765d6dcfb3226e55d55feb93d0d9eb46b1b2c01
mirmik <mirmikns@yandex.ru>
2022-03-25 11:00:38 +0300

    restore fd

--- nos/util/fd.h
+++ nos/util/fd.h
@@ -2,21 +2,22 @@
 #define NOS_UTIL_FD_H
 
 #include <signal.h>
-//#include <unistd.h>
-//#include <fcntl.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 namespace nos
 {
 	namespace osutil
 	{
-		static int nonblock(int, bool)
+		static int nonblock(int fd, bool en)
 		{
-                        /*if (fd < 0) return -1;
+            if (fd < 0) 
+            	return -1;
 			int flags = fcntl(fd, F_GETFL, 0);
-			if (flags < 0) return -1;
+			if (flags < 0) 
+				return -1;
 			flags = en ? (flags | O_NONBLOCK) : (flags & ~O_NONBLOCK);
-                        return fcntl(fd, F_SETFL, flags) == 0;*/
-                    return 0;
+            return fcntl(fd, F_SETFL, flags) == 0;
 		}
 
 	}
