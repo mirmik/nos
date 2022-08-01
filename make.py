523042136db47ea346a8f5db510a2536c247ac33
mirmik <mirmikns@yandex.ru>
2022-08-02 02:51:50 +0300

    'test move'

--- make.py
+++ make.py
@@ -30,9 +30,23 @@ licant.cxx_static_and_shared("libraries",
 	libs=["ws2_32"] if sys.platform=="win32" else [],
 )
 
+licant.cxx_application("runtests",
+            sources=["tests/*.cpp"],
+            ld_flags="-L/usr/local/lib/",
+            include_paths=["tests/."],
+            mdepends=["nos", "nos.shell.standart", "nos.inet"],
+            cxxstd="c++2a -Weffc++",
+            ccstd="c11",
+            cxx_flags="-g -O0 -Werror=all -Werror=pedantic -Wno-gnu-zero-variadic-macro-arguments",
+            cc_flags="-g -O0 -Werror=all -Werror=pedantic -Wno-gnu-zero-variadic-macro-arguments",
+            libs=["pthread"],
+            )
+
 licant.install.install_library(	tgt="install", 
 								libtgt=["libnos.a", "libnos.so"], 
 								headers="nos", 
 								hroot="nos")
 	
-licant.ex("libraries")
+licant.fileset("all", targets=["runtests", "libnos.a", "libnos.so"])
+
+licant.ex("all")
