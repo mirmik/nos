8a9b3a55a4d83b62c081b5461e404a5a7abffb7f
mirmik <mirmikns@yandex.ru>
2022-08-02 03:15:17 +0300

    'a'

--- tools/make-dist.sh
+++ tools/make-dist.sh
@@ -1,3 +1,11 @@
 docker build . -t arm32v7/nos -f tools/dockerfile.arch --build-arg ARCH=arm32v7
 docker build . -t arm64v8/nos -f tools/dockerfile.arch --build-arg ARCH=arm64v8
 docker build . -t nos -f tools/dockerfile.host
+
+docker run --rm --entrypoint cat nos /root/nos/libnos.so > dist/libnos.so
+docker run --rm --entrypoint cat arm32v7/nos /root/nos/libnos.so > dist/arm32v7/libnos.so
+docker run --rm --entrypoint cat arm64v8/nos /root/nos/libnos.so > dist/arm64v8/libnos.so
+
+docker run --rm --entrypoint cat nos /root/nos/libnos.a > dist/libnos.a
+docker run --rm --entrypoint cat arm32v7/nos /root/nos/libnos.a > dist/arm32v7/libnos.a
+docker run --rm --entrypoint cat arm64v8/nos /root/nos/libnos.a > dist/arm64v8/libnos.a
