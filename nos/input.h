2959a6ec54f8272286657c88167b36320f8687e6
mirmik <mirmikns@yandex.ru>
2022-08-16 16:13:28 +0300

    'read_from'

--- nos/input.h
+++ nos/input.h
@@ -3,24 +3,32 @@
 #ifndef NOS_INPUT_H
 #define NOS_INPUT_H
 
-#include <string>
 #include <stdlib.h>
+#include <string>
 
-namespace nos 
+namespace nos
 {
-	class istream;
-	extern istream* current_istream;
-	
-	std::string readline_from(nos::istream& is);
-	int read_until(nos::istream& is, char* buf, size_t buflen, char delim);
-	int read_paired(nos::istream& is, char* buf, size_t buflen, char a, char b, bool ignore=true);
+    class istream;
+    extern istream *current_istream;
 
-	int read_with_timeout_ms(nos::istream& is, char* buf, size_t buflen, int timeout_ms);
-	std::string readall_from(nos::istream& is);
-	std::string readline();
+    std::string read_from(nos::istream &is, size_t sz);
+    std::string readline_from(nos::istream &is);
+    int read_until(nos::istream &is, char *buf, size_t buflen, char delim);
+    int read_paired(nos::istream &is,
+                    char *buf,
+                    size_t buflen,
+                    char a,
+                    char b,
+                    bool ignore = true);
 
-	[[deprecated]]
-	std::string readline(nos::istream& is);		
+    int read_with_timeout_ms(nos::istream &is,
+                             char *buf,
+                             size_t buflen,
+                             int timeout_ms);
+    std::string readall_from(nos::istream &is);
+    std::string readline();
+
+    [[deprecated]] std::string readline(nos::istream &is);
 }
 
 #include <nos/io/istream.h>
--- nos/input/input.cpp
+++ nos/input/input.cpp
@@ -2,147 +2,160 @@
 #include <nos/io/istream.h>
 #include <stdexcept>
 
-std::string nos::readline_from(nos::istream& is)
+std::string nos::readline_from(nos::istream &is)
 {
-	char c;
-	std::string ret;
+    char c;
+    std::string ret;
 
-	while (1)
-	{
-		int readed = is.read(&c, 1);
+    while (1)
+    {
+        int readed = is.read(&c, 1);
 
-		if (readed < 0)
-		{
-			throw std::runtime_error("read error");
-		}
+        if (readed < 0)
+        {
+            throw std::runtime_error("read error");
+        }
 
-		if (readed == 0)
-		{
-			return ret;
-		}
+        if (readed == 0)
+        {
+            return ret;
+        }
 
-		ret.push_back(c);
+        ret.push_back(c);
 
-		if (c == '\r')
-		{
-			continue;
-		}
+        if (c == '\r')
+        {
+            continue;
+        }
 
-		if (c == '\n')
-		{
-			return ret;
-		}
-	}
+        if (c == '\n')
+        {
+            return ret;
+        }
+    }
 }
 
-std::string nos::readline(nos::istream& is)
+std::string nos::readline(nos::istream &is)
 {
-	return nos::readline_from(is);
+    return nos::readline_from(is);
 }
 
-int nos::read_until(nos::istream& is, char* buf, size_t buflen, char delim)
+int nos::read_until(nos::istream &is, char *buf, size_t buflen, char delim)
 {
-	char c;
-	int count = 0;
-	char* last = buf + buflen - 1;
+    char c;
+    int count = 0;
+    char *last = buf + buflen - 1;
 
-	while (buf != last)
-	{
-		int readed = is.read(&c, 1);
+    while (buf != last)
+    {
+        int readed = is.read(&c, 1);
 
-		if (readed < 0)
-		{
-			return readed;
-		}
+        if (readed < 0)
+        {
+            return readed;
+        }
 
-		if (readed == 0)
-		{
-			*buf = 0;
-			return count;
-		}
+        if (readed == 0)
+        {
+            *buf = 0;
+            return count;
+        }
 
-		*buf++ = c;
-		count++;
+        *buf++ = c;
+        count++;
 
-		if (c == delim)
-		{
-			*buf = 0;
-			return count;
-		}
-	}
+        if (c == delim)
+        {
+            *buf = 0;
+            return count;
+        }
+    }
 
-	//*buf = 0;
-	return count;
+    //*buf = 0;
+    return count;
 }
 
-/// Считывает из потока ввода содержимое строки между открывающейся скобкой 'a' и закрывающейся скобкой 'b'
-/// Параметр ignore позволяет отбростить данные перед первой скобкой.
-int nos::read_paired(nos::istream& is, char* buf, size_t buflen, char a, char b, bool ignore)
+/// Считывает из потока ввода содержимое строки между открывающейся скобкой 'a'
+/// и закрывающейся скобкой 'b' Параметр ignore позволяет отбростить данные
+/// перед первой скобкой.
+int nos::read_paired(
+    nos::istream &is, char *buf, size_t buflen, char a, char b, bool ignore)
 {
-	char c;
-	int count = 0;
-	char* last = buf + buflen - 1;
+    char c;
+    int count = 0;
+    char *last = buf + buflen - 1;
 
-	int paircount = 0;
+    int paircount = 0;
 
-	if (ignore)
-		do
-		{
-			int ret = is.read(&c, 1);
-			if (ret <= 0) return ret;
-		}
-		while (c != a);
-	else
-	{
-		is.read(&c, 1);
-		if (c != a) return -1;
-	}
+    if (ignore)
+        do
+        {
+            int ret = is.read(&c, 1);
+            if (ret <= 0)
+                return ret;
+        } while (c != a);
+    else
+    {
+        is.read(&c, 1);
+        if (c != a)
+            return -1;
+    }
 
+    paircount = 1;
+    *buf++ = c;
+    count++;
 
-	paircount = 1;
-	*buf++ = c;
-	count++;
+    while (paircount != 0 && buf != last)
+    {
+        int readed = is.read(&c, 1);
 
-	while (paircount != 0 && buf != last)
-	{
-		int readed = is.read(&c, 1);
+        if (readed <= 0)
+        {
+            *buf = 0;
+            return count;
+        }
 
-		if (readed <= 0)
-		{
-			*buf = 0;
-			return count;
-		}
+        if (c == a)
+        {
+            ++paircount;
+        }
 
-		if (c == a)
-		{
-			++paircount;
-		}
+        if (c == b)
+        {
+            if (--paircount == 0)
+            {
+                *buf++ = c;
+                count++;
+                *buf = 0;
+                return count;
+            }
+        }
 
-		if (c == b)
-		{
-			if (--paircount == 0)
-			{
-				*buf++ = c;
-				count++;
-				*buf = 0;
-				return count;
-			}
-		}
+        *buf++ = c;
+        count++;
+    }
 
-		*buf++ = c;
-		count++;
-	}
-
-	*buf = 0;
-	return count;
+    *buf = 0;
+    return count;
 }
 
-std::string nos::readall_from(nos::istream& is)
+std::string nos::readall_from(nos::istream &is)
 {
-	return is.readall();
+    return is.readall();
 }
 
-std::string nos::readline() 
+std::string nos::readline()
 {
-	return nos::readline(*nos::current_istream);
+    return nos::readline(*nos::current_istream);
+}
+
+std::string read_from(nos::istream &is, size_t sz)
+{
+    std::string ret;
+    ret.resize(sz);
+    int size = is.read(&ret[0], sz);
+    if (size < 0)
+        throw std::runtime_error("read error");
+    ret.resize(size);
+    return ret;
 }
