b8b2301cd718a5d0f1fe63264748f15c51094b6b
mirmik <mirmikns@yandex.ru>
2022-08-26 16:32:58 +0300

    'add logger level test'

--- tests/log.cpp
+++ tests/log.cpp
@@ -10,3 +10,15 @@ TEST_CASE("nos.log")
     CHECK_EQ(ss.str(), "[ info] Hello, world!\n");
     nos::current_ostream = nullptr;
 }
+
+TEST_CASE("logging level")
+{
+    nos::stringstream ss;
+    nos::current_ostream = &ss;
+    auto logger = nos::log::logger("logger").set_level(nos::log::level::Info);
+    logger.log(nos::log::level::Debug, "Hello, world!");
+    CHECK_EQ(ss.str(), "");
+    logger.set_level(nos::log::level::Debug);
+    logger.log(nos::log::level::Debug, "Hello, world!");
+    CHECK_EQ(ss.str(), "[debug] Hello, world!\n");
+}
