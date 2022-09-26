f60b1edb0b6cb8117444de83be75c043f9b800d5
mirmik <mirmikns@yandex.ru>
2022-09-26 13:32:58 +0300

    'Macro protector renamed'

--- nos/util/macro.h
+++ nos/util/macro.h
@@ -1,5 +1,5 @@
-#ifndef NOS_UTIL_MACRO_H
-#define NOS_UTIL_MACRO_H
+#ifndef NOS_UTIL2_MACRO_H
+#define NOS_UTIL2_MACRO_H
 
 #define NOS_STRINGIFY(...) #__VA_ARGS__
 #define NOS_STRINGIFY2(...) STRINGIFY(__VA_ARGS__)
