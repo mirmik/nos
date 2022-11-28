477618cd6295bc43355d2271d2c0408178b5217b
mirmik <mirmikns@yandex.ru>
2022-11-28 23:33:42 +0300

    'size helper is deprecated for now'

--- nos/util/size.h
+++ nos/util/size.h
@@ -8,26 +8,28 @@
 
 namespace nos
 {
-    template <class C> constexpr auto size(const C &c) -> decltype(c.size())
+    template <class C>
+    [[deprecated]] constexpr auto size(const C &c) -> decltype(c.size())
     {
         return c.size();
     }
 
     template <class C>
-    constexpr auto ssize(const C &c) -> decltype(c.size() - c.size())
+    [[deprecated]] constexpr auto ssize(const C &c)
+        -> decltype(c.size() - c.size())
     {
         using R = decltype(c.size() - c.size());
         return static_cast<R>(c.size());
     }
 
     template <class T, std::size_t N>
-    constexpr std::size_t size(const T (&array)[N]) noexcept
+    [[deprecated]] constexpr std::size_t size(const T (&)[N]) noexcept
     {
         return N;
     }
 
     template <class T, int N>
-    constexpr auto ssize(const T (&array)[N]) noexcept
+    [[deprecated]] constexpr auto ssize(const T (&)[N]) noexcept
         -> decltype(std::size_t() - std::size_t())
     {
         return N;
