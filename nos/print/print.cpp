b22295d906db649566b148dc1128e3955d04d717
mirmik <mirmikns@yandex.ru>
2023-03-01 16:38:38 +0300

    'printbin'

--- nos/print/print.cpp
+++ nos/print/print.cpp
@@ -19,10 +19,13 @@ nos::expected<size_t, nos::output_error> nos::putbyte_to(nos::ostream &out,
 nos::expected<size_t, nos::output_error>
 nos::writeln_to(nos::ostream &out, const void *buf, size_t sz)
 {
-    int ret = 0;
-    ret += out.write(buf, sz);
-    ret += nos::println_to(out);
-    return ret;
+    auto ret1 = out.write(buf, sz);
+    if (!ret1)
+        return ret1;
+    auto ret2 = nos::println_to(out);
+    if (!ret2)
+        return ret2;
+    return *ret1 + *ret2;
 }
 
 nos::expected<size_t, nos::output_error> nos::println_to(nos::ostream &o)
@@ -53,10 +56,10 @@ nos::expected<size_t, nos::output_error> nos::print_dump_to(
 
     if (len == 0)
     {
-        ret += out.write("0x", 2);
-        ret += nos::printptr_to(out, (void *)((char *)mem));
-        ret += out.putbyte(':');
-        ret += nos::println_to(out);
+        ret += *out.write("0x", 2);
+        ret += *nos::printptr_to(out, (void *)((char *)mem));
+        ret += *out.putbyte(':');
+        ret += *nos::println_to(out);
         return ret;
     }
 
@@ -66,21 +69,21 @@ nos::expected<size_t, nos::output_error> nos::print_dump_to(
         // print offset
         if (i % columns == 0)
         {
-            ret += out.write("0x", 2);
-            ret += nos::printptr_to(out, (void *)((char *)mem + i));
-            ret += out.putbyte(':');
+            ret += *out.write("0x", 2);
+            ret += *nos::printptr_to(out, (void *)((char *)mem + i));
+            ret += *out.putbyte(':');
         }
 
         // print hex data
         if (i < len)
         {
-            ret += nos::printhex_to(out, ((char *)mem)[i]);
-            ret += out.putbyte(' ');
+            ret += *nos::printhex_to(out, ((char *)mem)[i]);
+            ret += *out.putbyte(' ');
         }
         else
         {
             // end of block, just aligning for ASCII dump
-            ret += out.write("   ", 3);
+            ret += *out.write("   ", 3);
         }
 
         // print ASCII dump
@@ -91,21 +94,21 @@ nos::expected<size_t, nos::output_error> nos::print_dump_to(
                 if (j >= len)
                 {
                     // end of block, not really printing
-                    ret += out.putbyte(' ');
+                    ret += *out.putbyte(' ');
                 }
                 else if (isprint(((char *)mem)[j]))
                 {
                     // printable char
-                    ret += out.putbyte((char)0xFF & ((char *)mem)[j]);
+                    ret += *out.putbyte((char)0xFF & ((char *)mem)[j]);
                 }
                 else
                 {
                     // other char
-                    ret += out.putbyte('.');
+                    ret += *out.putbyte('.');
                 }
             }
 
-            ret += nos::println();
+            ret += *nos::println();
         }
     }
 
@@ -144,17 +147,29 @@ nos::expected<size_t, nos::output_error> nos::writeln(const void *buf,
 nos::expected<size_t, nos::output_error>
 nos::fill_to(nos::ostream &out, char c, size_t sz)
 {
+    size_t ret = 0;
     for (size_t i = 0; i < sz; i++)
-        out.putbyte(c);
-    return sz;
+    {
+        auto r = out.putbyte(c);
+        if (!r)
+            return r;
+        ret += *r;
+    }
+    return ret;
 }
 
 nos::expected<size_t, nos::output_error>
 nos::fill_to(nos::ostream &out, std::string_view &c, size_t sz)
 {
+    size_t ret = 0;
     for (size_t i = 0; i < sz; i++)
-        nos::print_to(out, c);
-    return c.size() * sz;
+    {
+        auto r = nos::print_to(out, c);
+        if (!r)
+            return r;
+        ret += *r;
+    }
+    return ret;
 }
 
 nos::expected<size_t, nos::output_error> nos::fill(char c, size_t sz)
@@ -182,7 +197,6 @@ nos::printhex_to(nos::ostream &out, const void *ptr, size_t sz)
 {
     size_t ret = 0;
     char *_ptr = (char *)ptr;
-
     for (int i = (int)sz - 1; i >= 0; i--)
     {
         auto ret1 = nos::printhex_to(out, _ptr[i]);
@@ -190,7 +204,6 @@ nos::printhex_to(nos::ostream &out, const void *ptr, size_t sz)
             return ret1.error();
         ret += ret1.value();
     }
-
     return ret;
 }
 
@@ -210,10 +223,13 @@ nos::printbin_to(nos::ostream &out, const void *ptr, size_t sz)
 {
     size_t ret = 0;
     char *_ptr = (char *)ptr;
-
     for (int i = (int)sz - 1; i >= 0; --i)
-        ret += printbin_to(out, _ptr[i]);
-
+    {
+        auto ret1 = printbin_to(out, _ptr[i]);
+        if (!ret1)
+            return ret1.error();
+        ret += ret1.value();
+    }
     return ret;
 }
 
