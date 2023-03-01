5631d2865b4b30394007bafeaee56bf72eacf974
mirmik <mirmikns@yandex.ru>
2023-03-01 16:33:41 +0300

    'printhex test'

--- tests/hex.cpp
+++ tests/hex.cpp
@@ -6,5 +6,14 @@ TEST_CASE("printhex")
 {
     nos::stringstream writer;
     nos::current_ostream = &writer;
-    nos::printhexln(0xABCD);
+    nos::printhex((uint16_t)0xABCD);
+    CHECK_EQ(writer.str(), "ABCD");
+}
+
+TEST_CASE("printhex")
+{
+    nos::stringstream writer;
+    nos::current_ostream = &writer;
+    nos::printhex((uint32_t)0xABCD);
+    CHECK_EQ(writer.str(), "0000ABCD");
 }
