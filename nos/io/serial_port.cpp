25650959c0db1b9c1caceaf535da1a7f58d1f258
mirmik <mirmikns@yandex.ru>
2023-12-28 15:19:17 +0300

    'serial_port compat with upper case'

--- nos/io/serial_port.cpp
+++ nos/io/serial_port.cpp
@@ -1,172 +1,183 @@
+#include <fcntl.h>
 #include <nos/io/serial_port.h>
+#include <nos/print.h>
 #include <termios.h>
 #include <unistd.h>
-#include <fcntl.h>
 
-int nos::serial_port::open(const char * path,
-                                unsigned int baud,
-                                char parity,
-                                uint8_t bytesize,
-                                uint8_t stopbits)
+int nos::serial_port::open(const char *path,
+                           unsigned int baud,
+                           char parity,
+                           uint8_t bytesize,
+                           uint8_t stopbits)
 {
-	(void) bytesize;
-	(void) stopbits;
+    (void)bytesize;
+    (void)stopbits;
 
-	int ret;
+    int ret;
 
-	int fd = nos::file::open(path, O_RDWR | O_NOCTTY);
-	if (fd < 0)
-	{
-		perror("serial::open");
-		exit(0);
-	}
+    int fd = nos::file::open(path, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror("serial::open");
+        exit(0);
+    }
 
-	struct termios tattr, orig;
-	ret = tcgetattr(fd, &orig);
-	if (ret < 0)
-	{
-		perror("serial::tcgetattr");
-		exit(0);
-	}
+    struct termios tattr, orig;
+    ret = tcgetattr(fd, &orig);
+    if (ret < 0)
+    {
+        perror("serial::tcgetattr");
+        exit(0);
+    }
 
-	tattr = orig;  /* copy original and then modify below */
+    tattr = orig; /* copy original and then modify below */
 
-	/* input modes - clear indicated ones giving: no break, no CR to NL,
-	   no parity check, no strip char, no start/stop output (sic) control */
-	tattr.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    /* input modes - clear indicated ones giving: no break, no CR to NL,
+       no parity check, no strip char, no start/stop output (sic) control */
+    tattr.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 
-	/* output modes - clear giving: no post processing such as NL to CR+NL */
-	tattr.c_oflag &= ~(OPOST);
+    /* output modes - clear giving: no post processing such as NL to CR+NL */
+    tattr.c_oflag &= ~(OPOST);
 
-	/* control modes - set 8 bit chars */
-	tattr.c_cflag |= (CS8);
+    /* control modes - set 8 bit chars */
+    tattr.c_cflag |= (CS8);
 
-	/* local modes - clear giving: echoing off, canonical off (no erase with
-	   backspace, ^U,...),  no extended functions, no signal chars (^Z,^C) */
-	tattr.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    /* local modes - clear giving: echoing off, canonical off (no erase with
+       backspace, ^U,...),  no extended functions, no signal chars (^Z,^C) */
+    tattr.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-	/* control chars - set return condition: min number of bytes and timer */
-	tattr.c_cc[VMIN] = 1; tattr.c_cc[VTIME] = 0; /* immediate - anything       */
+    /* control chars - set return condition: min number of bytes and timer */
+    tattr.c_cc[VMIN] = 1;
+    tattr.c_cc[VTIME] = 0; /* immediate - anything       */
 
-	switch (parity)
-	{
-		case UART_PARITY_NONE:
-			tattr.c_cflag &= ~(PARENB);
-			tattr.c_cflag &= ~(PARODD);
-			break;
+    parity = tolower(parity);
 
-		case UART_PARITY_EVEN:
-			tattr.c_cflag |= (PARENB);
-			tattr.c_cflag &= ~(PARODD);
-			break;
+    switch (parity)
+    {
+    case UART_PARITY_NONE:
+        tattr.c_cflag &= ~(PARENB);
+        tattr.c_cflag &= ~(PARODD);
+        break;
 
-		case UART_PARITY_ODD:
-			tattr.c_cflag |= (PARENB);
-			tattr.c_cflag |= (PARODD);
-			break;
+    case UART_PARITY_EVEN:
+        tattr.c_cflag |= (PARENB);
+        tattr.c_cflag &= ~(PARODD);
+        break;
 
-		default:
-			return -1;
-	}
+    case UART_PARITY_ODD:
+        tattr.c_cflag |= (PARENB);
+        tattr.c_cflag |= (PARODD);
+        break;
 
-	if (baud == 115200)
-	{
-		cfsetispeed(&tattr, B115200);
-		cfsetospeed(&tattr, B115200);
-	}
-	else
-	{
-		return -1;
-	}
+    default:
+    {
+        nos::println("UART parity error");
+        return -1;
+    }
+    }
 
-	/* put terminal in raw mode after flushing */
-	ret = tcsetattr(fd, TCSAFLUSH, &tattr);
+    if (baud == 115200)
+    {
+        cfsetispeed(&tattr, B115200);
+        cfsetospeed(&tattr, B115200);
+    }
+    else
+    {
+        nos::println("baud != 115200 ???");
+        return -1;
+    }
 
-	if (ret < 0)
-	{
-		return -1;
-	}
+    /* put terminal in raw mode after flushing */
+    ret = tcsetattr(fd, TCSAFLUSH, &tattr);
 
-	return 0;
+    if (ret < 0)
+    {
+        nos::println("tcsetattr attr");
+        return -1;
+    }
+
+    return 0;
 }
 
-int nos::serial_port::setup(
-                                unsigned int baud,
-                                char parity,
-                                uint8_t bytesize,
-                                uint8_t stopbits)
+int nos::serial_port::setup(unsigned int baud,
+                            char parity,
+                            uint8_t bytesize,
+                            uint8_t stopbits)
 {
-	(void) bytesize;
-	(void) stopbits;
+    (void)bytesize;
+    (void)stopbits;
 
-	int ret;
-	int fd = nos::file::fd();
+    int ret;
+    int fd = nos::file::fd();
 
-	struct termios tattr, orig;
-	ret = tcgetattr(fd, &orig);
-	if (ret < 0)
-	{
-		perror("serial::tcgetattr");
-		exit(0);
-	}
+    struct termios tattr, orig;
+    ret = tcgetattr(fd, &orig);
+    if (ret < 0)
+    {
+        perror("serial::tcgetattr");
+        exit(0);
+    }
 
-	tattr = orig;  /* copy original and then modify below */
+    tattr = orig; /* copy original and then modify below */
 
-	/* input modes - clear indicated ones giving: no break, no CR to NL,
-	   no parity check, no strip char, no start/stop output (sic) control */
-	tattr.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    /* input modes - clear indicated ones giving: no break, no CR to NL,
+       no parity check, no strip char, no start/stop output (sic) control */
+    tattr.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 
-	/* output modes - clear giving: no post processing such as NL to CR+NL */
-	tattr.c_oflag &= ~(OPOST);
+    /* output modes - clear giving: no post processing such as NL to CR+NL */
+    tattr.c_oflag &= ~(OPOST);
 
-	/* control modes - set 8 bit chars */
-	tattr.c_cflag |= (CS8);
+    /* control modes - set 8 bit chars */
+    tattr.c_cflag |= (CS8);
 
-	/* local modes - clear giving: echoing off, canonical off (no erase with
-	   backspace, ^U,...),  no extended functions, no signal chars (^Z,^C) */
-	tattr.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    /* local modes - clear giving: echoing off, canonical off (no erase with
+       backspace, ^U,...),  no extended functions, no signal chars (^Z,^C) */
+    tattr.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-	/* control chars - set return condition: min number of bytes and timer */
-	tattr.c_cc[VMIN] = 1; tattr.c_cc[VTIME] = 0; /* immediate - anything       */
+    /* control chars - set return condition: min number of bytes and timer */
+    tattr.c_cc[VMIN] = 1;
+    tattr.c_cc[VTIME] = 0; /* immediate - anything       */
 
-	switch (parity)
-	{
-		case UART_PARITY_NONE:
-			tattr.c_cflag &= ~(PARENB);
-			tattr.c_cflag &= ~(PARODD);
-			break;
+    parity = tolower(parity);
 
-		case UART_PARITY_EVEN:
-			tattr.c_cflag |= (PARENB);
-			tattr.c_cflag &= ~(PARODD);
-			break;
+    switch (parity)
+    {
+    case UART_PARITY_NONE:
+        tattr.c_cflag &= ~(PARENB);
+        tattr.c_cflag &= ~(PARODD);
+        break;
 
-		case UART_PARITY_ODD:
-			tattr.c_cflag |= (PARENB);
-			tattr.c_cflag |= (PARODD);
-			break;
+    case UART_PARITY_EVEN:
+        tattr.c_cflag |= (PARENB);
+        tattr.c_cflag &= ~(PARODD);
+        break;
 
-		default:
-			return -1;
-	}
+    case UART_PARITY_ODD:
+        tattr.c_cflag |= (PARENB);
+        tattr.c_cflag |= (PARODD);
+        break;
 
-	if (baud == 115200)
-	{
-		cfsetispeed(&tattr, B115200);
-		cfsetospeed(&tattr, B115200);
-	}
-	else
-	{
-		return -1;
-	}
+    default:
+        return -1;
+    }
 
-	/* put terminal in raw mode after flushing */
-	ret = tcsetattr(fd, TCSAFLUSH, &tattr);
+    if (baud == 115200)
+    {
+        cfsetispeed(&tattr, B115200);
+        cfsetospeed(&tattr, B115200);
+    }
+    else
+    {
+        return -1;
+    }
 
-	if (ret < 0)
-	{
-		return -1;
-	}
+    /* put terminal in raw mode after flushing */
+    ret = tcsetattr(fd, TCSAFLUSH, &tattr);
 
-	return 0;
+    if (ret < 0)
+    {
+        return -1;
+    }
+
+    return 0;
 }
